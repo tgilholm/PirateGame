a5ce55be050f160f74d89f13aab9c675fd210c4c
tom <tgilholm@gmail.com>
2026-02-23 10:42:14 +0000

    Updated entity factory

--- PirateGame/server/server/src/entities/entity-factory.ts
+++ PirateGame/server/server/src/entities/entity-factory.ts
@@ -1,10 +1,27 @@
+import NPC from "./npc";
+import Player from "./player";
+
 /**
  * Aggregates entity creation, applying domain-specific default values from
  * the passed-in JSON
  */
 export default class EntityFactory {
 
-    constructor(private entityConfig: any) {}
+    entityConfig: any = JSON.parse("entity-config.json");
 
+    constructor() {}
+
+    public createPlayer(id: string, x: number, y: number, parentId: string | null, username: string): Player {
+        const playerConfig = this.entityConfig.player
+        return new Player(id, x, y, parentId, username, playerConfig.max_health);
+    }
+
+    // public createShip(): Ship {
+
+    // }
+
+    // public createNPC(): NPC {
+    //     return new 
+    // }
 
 }
