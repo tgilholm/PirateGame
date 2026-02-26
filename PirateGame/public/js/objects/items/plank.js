4c33ec5190f38511943c54874bc91ddbbb1cb550
bwabwo <samtobinwildman@hotmail.com>
2026-02-26 18:45:01 +0000

    removed plank item, we dont need this

--- PirateGame/public/js/objects/items/plank.js
+++
@@ -1,8 +0,0 @@
-export default class Plank extends Phaser.GameObjects.Sprite { //placeholder class for items, plank not used now but can become anything
-    constructor(scene, x, y) {
-        super(scene, x, y, 'plank');
-        this.scene = scene;
-        this.scene.add.existing(this);
-        this.setDepth(100);
-    }
-}
