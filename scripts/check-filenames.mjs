8b33963695083308e1380c7c335d7ef8e38c56cc
tgilholm <tgilholm@gmail.com>
2026-03-02 22:50:44 +0000

    Removed redundant files

--- scripts/check-filenames.mjs
+++ scripts/check-filenames.mjs
@@ -11,14 +11,14 @@ files.forEach((f) => {
     if (fname.startsWith('.')) return;
 
     if (!regex.test(fname)) {
-        console.error(`\x1b[31m%s\x1b[0m`, `Error: "${fname}" is not kebab-case.`);
+        console.error(`Error: "${fname}" is not kebab-case.`);
         console.error(`   Rename to: ${fname.toLowerCase().replace(/_/g, '-').replace(/\s+/g, '-')}`);
         code = 1;
     }
 });
 
 if (code === 0) {
-    console.log('\x1b[32m%s\x1b[0m', 'All filenames are valid.');
+    console.log('All filenames are valid.');
 }
 
 process.exit(code);
