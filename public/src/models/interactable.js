d9a51a51f0075becca9bbeb8a8af9cf02bbe98bb
tgilholm <tgilholm@gmail.com>
2026-03-05 13:30:32 +0000

    Added JSDoc to model classes

--- public/src/models/interactable.js
+++ public/src/models/interactable.js
@@ -1,6 +1,19 @@
+import ShipModel from "./ship-model";
+
+/**
+ * Client side interactable entity. Contains prompts for user interaction.
+ * Can be added to ships by adding to their containers.
+ */
 export default class Interactable extends Phaser.GameObjects.Sprite {
+
+    /**
+     * Builds an interactable object in the specified scene with an optional parent
+     * @param {Phaser.Scene} scene the scene to add this entity to
+     * @param {ShipModel | null} parent the (optional) parent ship to add this entity to
+     * @param {Object} config the configuration data to create this entity from
+     */
     constructor(scene, parent, config) {
-        super(scene, config.x, config.y, config.texture);
+        super(scene, config.x, config.y, config.texture);   // Specify start coords and texture name in .json
 
         this.id = config.id;    // default value
         this.type = config.type;
@@ -10,6 +23,7 @@ export default class Interactable extends Phaser.GameObjects.Sprite {
 
         scene.add.existing(this);
 
+        // Responds to the side of the ship this entity is on
         if (parent) {
             this.parentId = parent.id;
             this.id = `${parent.id}_${config.id}`;
@@ -17,15 +31,19 @@ export default class Interactable extends Phaser.GameObjects.Sprite {
             parent.add(this);
 
             if (this.type !== "helm") {
-                if (config.y < 0) {
+                if (config.y < 0) {     // "port" side- keep upright
                     this.setRotation(0);
                 } else {
-                    this.setRotation(Math.PI);
+                    this.setRotation(Math.PI); // "starboard" - flip in Y
                 }
             }
         }
     }
 
+    /**
+     * Gets the absolute coordinates of this object, ignoring any parent containers 
+     * @returns the x and y coordinates of this Interactable
+     */
     getWorldPosition() {
         const matrix = this.getWorldTransformMatrix();
         return { x: matrix.tx, y: matrix.ty };
--- public/src/models/ship-model.js
+++ public/src/models/ship-model.js
@@ -3,33 +3,31 @@
 import Interactable from "./interactable.js";
 
 /**
- * The Ship class provides a moving body on which Interactables and Players can exist.
- * It can move independently and all objects "attached" to it will move with it- players'
- * movement is added on to the ship's movement so that in the world space, players move
- * independently of the ship.
- * 
+ * Client-side representation of ship data. Extends container to allow other 
+ * game objects to move with, and independently of the ship.  
  */
 export default class ShipModel extends Phaser.GameObjects.Container {
 
     /**
-     * @param {Phaser.Scene} scene
-     * @param {string} id
-     * @param {number} x
-     * @param {number} y
-     * @param {ShipConfig} config
+     * Creates a ship in the specified scene at the provided coordinates.
+     * @param {Phaser.Scene} scene the scene to add this ship to
+     * @param {string} id the id of the ship
+     * @param {number} x the (absolute) x coordinate to add this ship to
+     * @param {number} y the (absolute) y coordinate to add this ship to
+     * @param {ShipConfig} config the config data specifying the ship's dimensions
      */
     constructor(scene, id, x, y, config) {
         super(scene, x, y);
-        this.dimensions = config.dimensions;
+        this.dimensions = config.dimensions; // destructure the config- we only need the hull dimensions
         this.interactables = [];
         this.id = id;
 
-
-        this.target = { x: 0, y: 0, r: 0 };
-        this.velocity = { x: 0, y: 0 };
+        this.target = { x: 0, y: 0, r: 0 }; // for interpolation
+        this.velocity = { x: 0, y: 0 }; // for extrapolation
         this.pilotId = null;
         this.angularVelocity = 0;
 
+        // Adds each of the interactables from the config to this ship
         if (config.interactables) {
             config.interactables.forEach(item => {
                 const model = new Interactable(this.scene, this, item);
@@ -37,32 +35,39 @@ export default class ShipModel extends Phaser.GameObjects.Container {
             })
         }
 
-
         this.hullSprite = null;
-        this.drawHull();
+        this.drawHull(this.dimensions);
         this.drawInteractables();
         this.setRotation(0);
         scene.add.existing(this);
     }
 
-    drawHull() {
-        if (!this.dimensions) return;
-        const { height, middleWidth, bowLength, sternRadius } = this.dimensions;
+
+    /**
+     * Creates the sprite for this ship using the dimensions provided in the config file
+     * @param {ShipConfig["dimensions"]} dimensions the dimensions of the ship
+     */
+    drawHull(dimensions) {
+        if (!dimensions) return;
+
+
+        const { height, middleWidth, bowLength, sternRadius } = dimensions;
         const halfH = height / 2;
-        const halfW = middleWidth / 2;
-        const segments = 12;
+        const halfW = middleWidth / 2;  // width in pixels of the middle rectangle
+        const segments = 12;            // how much to subdivide the line by
         const padding = 5;
-        const totalW = middleWidth + bowLength + sternRadius + (padding * 2);
+        const totalW = middleWidth + bowLength + sternRadius + (padding * 2);   // total length of the ship
         const totalH = height + (padding * 2);
         const offsetX = sternRadius + halfW + padding;
         const offsetY = halfH + padding;
 
+        // Draw the ship with an outline, fill with brown
         const graphics = this.scene.make.graphics({ x: 0, y: 0 }, false);
         graphics.fillStyle(0x5d4037, 1);
         graphics.lineStyle(4, 0xffffff, 1);
         graphics.beginPath();
 
-        // Stern
+        // Stern- draws a semi-circle 
         for (let i = 0; i <= segments; i++) {
             const theta = (Math.PI / 2) + (i / segments) * Math.PI;
             const px = offsetX + (-halfW + (Math.cos(theta) * sternRadius));
@@ -71,7 +76,7 @@ export default class ShipModel extends Phaser.GameObjects.Container {
             else graphics.lineTo(px, py);
         }
 
-        // Bow top
+        // Bow top- half of the quadratic curve
         for (let i = 0; i <= segments; i++) {
             const t = i / segments;
             const px = offsetX + (halfW + (t * bowLength));
@@ -79,7 +84,7 @@ export default class ShipModel extends Phaser.GameObjects.Container {
             graphics.lineTo(px, py);
         }
 
-        // Bow bottom
+        // Bow bottom - other half of the quadratic curve
         for (let i = segments; i >= 0; i--) {
             const t = i / segments;
             const px = offsetX + (halfW + (t * bowLength));
@@ -91,38 +96,69 @@ export default class ShipModel extends Phaser.GameObjects.Container {
         graphics.fillPath();
         graphics.strokePath();
 
+        // Only generate the texture if it doesn't already exist- future ships re-use this
         const textureName = 'hull';
         if (!this.scene.textures.exists(textureName)) {
             graphics.generateTexture(textureName, totalW, totalH);
         }
 
-
-        // Create sprite and set origin to the relative center
         if (this.hullSprite) this.hullSprite.destroy();
 
-
+        // Sets the origin point of this ship to the relative centre
         this.hullSprite = this.scene.add.sprite(0, 0, textureName);
         this.hullSprite.setOrigin(offsetX / totalW, offsetY / totalH);
         this.add(this.hullSprite);
         this.sendToBack(this.hullSprite);
         this.setDepth(10);
 
-        this.swayTimer = Math.random() * 1000;
+        this.swayTimer = Math.random() * 1000;  // For the random sine-wave "bobbing"
     }
 
+    /**
+     * Adds each of the interactable sprites to this container
+     */
     drawInteractables() {
-        // Get interactable positions from dimensions and create sprites for them
         this.interactables.forEach(i => {
             const item = this.add(i);
         });
     }
 
+    /**
+     * Updates the target state of this ship entity to the data provided from the server.
+     * @param {Object} data the data for this ship
+     */
+    syncFromServer(data) {
+        // Snap if this is the first sync or position has drifted too far
+        if (!this.initialised || Phaser.Math.Distance.Between(this.x, this.y, data.x, data.y) > 150) {
+            this.x = data.x;
+            this.y = data.y;
+            this.rotation = data.r;
+            this.initialised = true;
+        }
 
+        // Set the target position
+        this.target.x = data.x;
+        this.target.y = data.y;
+        this.target.r = data.r;
 
-    update(data, delta) {
-        if (data) this.syncFromServer(data);
+        // Get the current velocity for extrapolation
+        this.velocity.x = data.vx;
+        this.velocity.y = data.vy;
+        this.angularVelocity = data.av;
+        this.pilotId = data.pilotId;    // If someone is at the helm
+    }
+
+    /**
+     * Updates the position of this ship and the entities inside it from the target
+     * data provided in syncFromServer(). Extrapolates the position from
+     * the current speed and the time since the last update to predict the movement of the ship
+     * without waiting for a server update.
+     * @param {number} delta 
+     */
+    update(delta) {
         if (!this.initialised) return;
 
+        // Move the ship up and down slightly to "bob" with the waves
         this.swayTimer += delta;
         const bobAmount = Math.sin(this.swayTimer / 1000) * 1;
         const rockAmount = Math.cos(this.swayTimer / 1500) * 0.02;
@@ -136,39 +172,26 @@ export default class ShipModel extends Phaser.GameObjects.Container {
             item.y = item.startY + bobAmount;
         });
 
+        // Calculate the amount to interpolate 
         const deltaTime = delta / 1000;
         const lerp = 1 - Math.pow(1 - 0.1, delta / 16.67);
 
+        // Use distance = speed * time to calculate the predicted destination
         const predictedX = this.target.x + this.velocity.x * deltaTime;
         const predictedY = this.target.y + this.velocity.y * deltaTime;
         const predictedR = this.target.r + this.angularVelocity * deltaTime;
 
+        // Move smoothly to the predicted position, avoiding round pixels
         this.x = Math.round(Phaser.Math.Linear(this.x, predictedX, lerp));
         this.y = Math.round(Phaser.Math.Linear(this.y, predictedY, lerp));
 
+        // Also interpolate the rotation
         this.rotation = Phaser.Math.Angle.RotateTo(
             this.rotation,
             predictedR,
             lerp
         );
     }
-    
-    syncFromServer(data) {
-        // Snap if this is the first sync or position has drifted too far
-        if (!this.initialised || Phaser.Math.Distance.Between(this.x, this.y, data.x, data.y) > 150) {
-            this.x = data.x;
-            this.y = data.y;
-            this.rotation = data.r;
-            this.initialised = true;
-        }
 
-        this.target.x = data.x;
-        this.target.y = data.y;
-        this.target.r = data.r;
 
-        this.velocity.x = data.vx;
-        this.velocity.y = data.vy;
-        this.angularVelocity = data.av;
-        this.pilotId = data.pilotId;
-    }
 }
