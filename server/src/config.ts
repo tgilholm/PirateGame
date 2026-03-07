2d9b352a397eb8307249f9675444e7f45cdc5636
tgilholm <tgilholm@gmail.com>
2026-03-07 12:51:27 +0000

    Added documentation

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,3 +1,4 @@
+// Defines the static constants for the server.
 export const CONFIG = {
     TICK_RATE: 20,
     NET_TICK_RATE: 20,
--- server/src/types.ts
+++ server/src/types.ts
@@ -1,5 +1,8 @@
 import entityConfig from "../../shared/entity-config.json"
 
+// Provides type hinting to both TS and JS files using the shared entity config
+// This makes it easier to use the entity config without needing to import it directly into the file being used
+
 export type EntityConfig = typeof entityConfig;
 export type PlayerConfig = typeof entityConfig["player"];
 export type ShipConfig = typeof entityConfig["ship"];
