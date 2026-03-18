a39f6b91dadb6a3d4f4d631f966175aa15aac2b7
Connor McCarthy <connor.t.k@icloud.com>
2026-03-18 13:26:06 +0000

    Branch Ver 0.3.0 - Added Text styling to death screen - Added click to respawn prompt, current non-functional

--- public/src/models/player-model.js
+++ public/src/models/player-model.js
@@ -90,6 +90,8 @@ export default class PlayerModel extends Model {
         }
 
 
+        // If players health is 0 or below, show the death screen
+        // TODO: pass the players gold/score for display on the death screen
         if (this.health <= 0) {
             this.scene.scene.start('DeathScene');
             console.log("you ded");
