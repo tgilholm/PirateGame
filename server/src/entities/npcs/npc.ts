012a9f2456453220638f7687246261e222d96cfc
tgilholm <tgilholm@gmail.com>
2026-04-08 23:52:06 +0100

    Added attack timer to NPCs

--- server/src/entities/npcs/npc.ts
+++ server/src/entities/npcs/npc.ts
@@ -4,7 +4,9 @@ export default class NPC extends Entity {
 	target: Entity | null = null; // who this npc is currently chasing
 	speed: number = 4;
 	detectionRadius: number;
-	attackDamage: number = 1;
+	attackTimer: number = 0;
+	attackTime: number = 500; // ms
+	attackDamage: number = 10;
 
 	constructor(id: string, type: string = 'npc', x: number, y: number, detectionRadius: number = 250) {
 		super(id, type, x, y, 75, null);
