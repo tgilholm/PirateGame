ae5e4e08dd4d2b831a477878ee5eac59dc62ca42
tgilholm <tgilholm@gmail.com>
2026-05-09 15:10:10 +0100

    Removed stray bracket

--- server/src/handlers/session-handler.ts
+++ server/src/handlers/session-handler.ts
@@ -27,12 +27,12 @@ export default class SessionHandler {
 	/**
 	 * Called by SocketService when a player says they are READY
 	 */
-	public addPlayer(socketId: string, username: string, pirateColour: string = 'default', shipChoice: number = 0)) {
+	public addPlayer(socketId: string, username: string, pirateColour: string = 'default', shipChoice: number = 0) {
 		const { x, y, rotation } = this.spawnSystem.getSpawnPoint();
 		const newShip = this.factory.createShip(`ship_${socketId}`, x, y, shipChoice);
 
 		Matter.Body.rotate(newShip.body, rotation * (Math.PI / 180));
-    
+
 		// "hacky" way of adding to the physics world
 		this.addPhysicsBody(newShip.body);
 		this.factory.createPlayer(socketId, 0, 0, newShip, username, pirateColour);
